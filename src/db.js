8213927453a5b4252e1e58cd8e0e8fbb177d6c19
pauljordantalbot <pauljordantalbot@gmail.com>
2023-06-09 13:56:08 -0400

    DB_URL updated

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'test') {
 	console.log('test database');
 	DB_URI = 'postgresql:///signwithme_test';
 } else if (process.env.NODE_ENV === 'production') {
-	DB_URI = 'postgresql:///signwithme';
+	DB_URI = process.env.DATABASE_URL;
 } else {
 	console.log('Dev DataBase');
 	DB_URI = 'postgresql:///signwithme_test';
